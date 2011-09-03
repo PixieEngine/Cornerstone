023a5346cc4eab37b402534eb0b2465bdcf8061c
condor <mdiebolt@gmail.com>
2011-09-03 03:43:28 -0400

    object_extensions

--- corelib.js
+++ corelib.js
@@ -1702,6 +1702,14 @@ Checks whether an object is an array.
 @param {Object} object The object to check for array-ness.
 @type Boolean
 @returns A boolean expressing whether the object is an instance of Array 
+
+<code><pre>
+   Object.isArray([1, 2, 4])
+=> true
+   
+   Object.isArray({key: "value"})
+=> false
+</pre></code>
 */var __slice = Array.prototype.slice;
 Object.isArray = function(object) {
   return Object.prototype.toString.call(object) === '[object Array]';
