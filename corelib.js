fba01394c258e6adbce6ea90113a05dcdadf7d05
condor <mdiebolt@gmail.com>
2011-09-02 22:30:46 -0400

    Modified in browser at pixie.strd6.com

--- corelib.js
+++ corelib.js
@@ -1,7 +1,12 @@
 ;
 ;
 /**
-Returns a copy of the array without null and undefined values..
+Returns a copy of the array without null and undefined values.
+
+<code><pre>
+   [null, undefined, 3, 3, undefined, 5].compact()
+=> [3, 3, 5]
+</pre></code>
 
 @name compact
 @methodOf Array#
