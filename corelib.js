11b765b2f540145feb5af3e9882eb813cd92a932
condor <mdiebolt@gmail.com>
2011-09-02 22:36:20 -0400

    Modified in browser at pixie.strd6.com

--- corelib.js
+++ corelib.js
@@ -69,6 +69,16 @@ Array.prototype.clear = function() {
 /**
 Flatten out an array of arrays into a single array of elements.
 
+<code><pre>
+
+   [[1, 2], [3, 4], 5].flatten()
+=> [1, 2, 3, 4, 5]
+
+   [[1, 2], [3, [4, 5]], 6].flatten()
+=> [1, 2, 3, [4, 5], 6]
+
+</pre></code>
+
 @name flatten
 @methodOf Array#
 @type Array
