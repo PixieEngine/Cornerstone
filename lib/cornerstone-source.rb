adad6e26faf6a574c0407b79ea711aff484ef0d5
Matt Diebolt <mdiebolt@gmail.com>
2013-02-16 18:45:10 -0600

    simplifying config

--- lib/cornerstone-source.rb
+++ lib/cornerstone-source.rb
@@ -1,13 +1,14 @@
 require "cornerstone-source/version"
 
-# Sneaky require for Rails engine environment
-if defined? ::Rails::Engine
-  require "cornerstone-source/rails"
-elsif defined? ::Sprockets
-  require "cornerstone-source/sprockets"
-end
-
 module Cornerstone
   module Source
+    if defined? ::Rails::Engine
+      class Rails < Rails::Engine
+      end
+    elsif defined? ::Sprockets
+      root_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
+
+      ::Sprockets.append_path File.join(root_dir, "source")
+    end
   end
 end
--- lib/cornerstone-source/version.rb
+++ lib/cornerstone-source/version.rb
@@ -1,5 +1,5 @@
 module Cornerstone
   module Source
-    VERSION = "0.1.6"
+    VERSION = "0.1.7"
   end
 end
