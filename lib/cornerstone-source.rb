19e2a5dee479bc97ad494e9c2f1f1bb2904b0f7a
Matt Diebolt <mdiebolt@gmail.com>
2013-02-17 13:05:03 -0600

    simplifying directory structure

--- lib/cornerstone-source.rb
+++ lib/cornerstone-source.rb
@@ -1,12 +1,9 @@
+require 'sprockets'
+
 module Cornerstone
   module Source
-    if defined? ::Rails::Engine
-      class Rails < Rails::Engine
-      end
-    elsif defined? ::Sprockets
-      root_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
+    root_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
 
-      ::Sprockets.append_path File.join(root_dir, "lib", "assets", "javascripts")
-    end
+    ::Sprockets.append_path File.join(root_dir, "source")
   end
 end
