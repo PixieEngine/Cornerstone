321e90b895e9aabf0f4cca6b87c575b9821ba477
Daniel Moore <yahivin@gmail.com>
2013-03-13 17:07:56 -0700

    Added support for Rails engines with non-standard assets path

--- lib/cornerstone-source.rb
+++ lib/cornerstone-source.rb
@@ -2,8 +2,15 @@ require 'sprockets'
 
 module Cornerstone
   module Source
-    root_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
+    if defined? ::Rails
+      class Engine < ::Rails::Engine
+        config.paths['app/assets'] = "source"
+      end
+    else
+      root_dir = File.expand_path(File.dirname(File.dirname(__FILE__)))
+      asset_dir = File.join(root_dir, "source")
 
-    ::Sprockets.append_path File.join(root_dir, "source")
+      ::Sprockets.append_path asset_dir
+    end
   end
 end
